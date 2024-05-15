8f4dd37b99892e57c834c5e56e55afb232bfd642
Suresh Prajapati <sureshprajapati076@gmail.com>
2024-05-15 08:17:37 -0700

    error handling fo deserializer

--- consumer/src/main/java/com/kafka/consumer/config/KafkaConsumerConfig.java
+++ consumer/src/main/java/com/kafka/consumer/config/KafkaConsumerConfig.java
@@ -10,6 +10,7 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
@@ -33,8 +34,19 @@ public class KafkaConsumerConfig {
     public Map<String,Object> consumerConfig(){
         Map<String,Object> props =  new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+//        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+//        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+
+
+        // Solution for error: This error handler cannot process 'SerializationException's directly; please consider configuring an 'ErrorHandlingDeserializer'
+        // link: https://stackoverflow.com/questions/70252047/spring-kafka-error-this-error-handler-cannot-process-serializationexceptions
+
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        props.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, StringDeserializer.class);
+        props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
+
+
 
         // Below config is used to map from producer dto to consumer dto in terms of packages...
         props.put(JsonDeserializer.TYPE_MAPPINGS,"com.kafka.producer.dto.Person:com.kafka.consumer.dto.Person,com.kafka.producer.model.MyCar:com.kafka.consumer.dto.MyCar");
